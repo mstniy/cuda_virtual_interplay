dd4da055545dfade403e00d73c1e8ce94287178d
Kartal Kaan Bozdoğan <kartalkaanbozdogan@gmail.com>
2020-04-16 22:09:31 +0300

    Stop using manual RTTI Parallelize the resuscitation of the objects. Launch one kernel per dynamic type instead of per object.

--- virtual.cu
+++ virtual.cu
@@ -2,6 +2,8 @@
 #include <vector>
 #include <memory>
 #include <iostream>
+#include <typeindex>
+#include <unordered_map>
 
 #define CUDA_CALL(x) do { if((x) != cudaSuccess) { \
     printf("Error at %s:%d\n",__FILE__,__LINE__); \
@@ -9,26 +11,27 @@
 
 using namespace std;
 
-enum class TypeId // CUDA does not support device-side RTTI
-{ //TODO: Can we get rid of this?
-	Sub1, Sub2
-};
-
 template<typename Base>
 class device_copyable
 {
 public:
 	virtual size_t getMostDerivedSize() const = 0;
 	virtual Base* placementNew(void* ptr) const = 0;
-	//virtual void copyToDevice(Base** pos) = 0;
+	virtual void resusciate(void** pos, int len) = 0;
 	virtual ~device_copyable() = default;
 };
 
-/*template<typename Base, typename Derived>
-__global__ void copy_to_device(Base** object, Derived h)
+template<typename Derived>
+__global__ void resuscitate_kernel(Derived** objects, int len)
 {
-	*object = new Derived(h);
-}*/
+	int tid = threadIdx.x + blockIdx.x * blockDim.x;
+	int thread_count = gridDim.x * blockDim.x;
+	for (int i=tid; i<len; i += thread_count)
+	{
+		Derived s(*static_cast<Derived*>(objects[i]));
+		new (objects[i]) Derived(s);
+	}
+}
 
 template<typename Base, typename Derived>
 class implements_device_copyable : virtual public device_copyable<Base>
@@ -44,20 +47,23 @@ public:
 		Derived* dp = reinterpret_cast<Derived*>(ptr);
 		return static_cast<Base*>(dp);
 	}
-	/*void copyToDevice(Base** pos) override
+	void resusciate(void** pos, int len) override
 	{
-		copy_to_device<Base, Derived><<<1,1>>>(pos, static_cast<Derived&>(*this));
-	}*/
+		const int TB_SIZE = 128;
+		const int THREAD_COUNT = 16384;
+		int grid_size = (THREAD_COUNT+TB_SIZE-1)/TB_SIZE;
+		resuscitate_kernel<Derived><<<grid_size, TB_SIZE>>>(reinterpret_cast<Derived**>(pos), len);
+		//cudaDeviceSynchronize();
+	}
 };
 
 class Base : public virtual device_copyable<Base>
 {
 public:
-	TypeId type_id;
 	int b_id;
 public:
-	__host__ __device__ Base(TypeId _type_id, int _b_id):type_id(_type_id), b_id(_b_id){}
-	__host__ __device__ Base(TypeId _type_id, const Base& b):type_id(_type_id), b_id(b.b_id){}
+	__host__ __device__ Base(int _b_id):b_id(_b_id){}
+	__host__ __device__ Base(const Base& b):b_id(b.b_id){}
 	virtual ~Base() = default;
 	__device__ virtual void f() = 0;
 };
@@ -67,12 +73,8 @@ class Sub1 : public Base, public implements_device_copyable<Base, Sub1>
 public:
 	int sub_id;
 public:
-	__host__ __device__ Sub1(int _b_id, int _sub_id):
-		Base(TypeId::Sub1, _b_id), sub_id(_sub_id)
-	{}
-	__host__ __device__ Sub1(const Sub1& s):
-		Base(TypeId::Sub1, s), sub_id(s.sub_id)
-	{}
+	__host__ __device__ Sub1(int _b_id, int _sub_id):Base(_b_id), sub_id(_sub_id){}
+	__host__ __device__ Sub1(const Sub1& s):Base(s), sub_id(s.sub_id){}
 	__device__ void f() override
 	{
 		printf("hello 1: %d %d!\n", b_id, sub_id);
@@ -84,38 +86,13 @@ class Sub2 : public Base, public implements_device_copyable<Base, Sub2>
 public:
 	char sub_ch;
 public:
-	__host__ __device__ Sub2(int _b_id, char _sub_ch):
-		Base(TypeId::Sub1, _b_id), sub_ch(_sub_ch)
-	{}
-	__host__ __device__ Sub2(const Sub2& s):
-		Base(TypeId::Sub2, s), sub_ch(s.sub_ch)
-	{}
+	__host__ __device__ Sub2(int _b_id, char _sub_ch):Base(_b_id), sub_ch(_sub_ch){}
+	__host__ __device__ Sub2(const Sub2& s):Base(s), sub_ch(s.sub_ch){}
 	__device__ void f() override
 	{
 		printf("hello 2: %d %c!\n", b_id, sub_ch);
 	}
 };
-
-__global__ void resuscitate(Base** objects, int len)
-{
-	for (int i=0; i<len; i++)
-	{
-		// TODO: Do not resolve RTTI on device side, as this will result in high divergence (UNLESS objects of the same type are next to one another)
-		//  Instead, resolve them on host side(how?)
-		//    Have a correcponsing resusciate for each MDT (template resusciate). Make it into a virtual function in device_copyable. In *run*, group objects by their MDT, and for each group, call its virtual resusciate, which'll in turn call the templated resusciate_kernel tailored for that BTC.
-		if (objects[i]->type_id == TypeId::Sub1)
-		{
-			Sub1 s(*static_cast<Sub1*>(objects[i]));
-			new (objects[i]) Sub1(s);
-		}
-		else if (objects[i]->type_id == TypeId::Sub2)
-		{
-			Sub2 s(*static_cast<Sub2*>(objects[i]));
-			new (objects[i]) Sub2(s);
-		}
-	}
-}
-
 
 __global__ void runner(Base** objects, int len)
 {
@@ -127,19 +104,43 @@ __global__ void runner(Base** objects, int len)
 
 void run(std::vector<std::unique_ptr<Base>> objs)
 {
-	Base** d_objects;
-	CUDA_CALL(cudaMallocManaged((void **)&d_objects, objs.size() * sizeof(Base*)));
+	// Calculate the total size required for the unified memory buffer
 	size_t totalSize=0, currentSize=0;
 	for (const auto& obj : objs)
 		totalSize += obj->getMostDerivedSize();
+	// Group objects of the same dynamic type together
+	std::unordered_map<std::type_index, std::vector<std::unique_ptr<Base>>> groups;
+	for (auto&& obj : objs)
+	{
+		groups[typeid(*obj)].push_back(std::move(obj));
+	}
+	// Allocate memory to store pointers to the objects
+	Base** d_objects;
+	int d_objects_index=0;
+	CUDA_CALL(cudaMallocManaged((void **)&d_objects, objs.size() * sizeof(Base*)));
+	// Allocate memory to store the objects
 	char* object_buffer;
 	CUDA_CALL(cudaMallocManaged((void **)&object_buffer, totalSize));
-	for (int i=0; i<objs.size(); i++)
+	// Copy the objects into unified memory
+	for (const auto& pr : groups)
 	{
-		d_objects[i] = objs[i]->placementNew(object_buffer + currentSize);
-		currentSize += objs[i]->getMostDerivedSize();
+		for (const auto& obj : pr.second)
+		{
+			d_objects[d_objects_index++] = obj->placementNew(object_buffer + currentSize);
+			currentSize += obj->getMostDerivedSize();
+		}
 	}
-	resuscitate<<<1,1>>>(d_objects, objs.size());
+	// Resuscitate the objects, one group at a time.
+	// Resuscitation lets the device code use the virtual functions of the objects (it probably fixes up the vtable? See https://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html#virtual-functions)
+	// We actually have a dedicated kernel for each dynamic type, which is what device_copyable::resusciate ends up running.
+	// This keeps us from having to maintain RTTI manually. It also reduces branch divergence on the device.
+	d_objects_index = 0;
+	for (const auto& pr : groups)
+	{
+		pr.second[0]->resusciate((void**)&d_objects[d_objects_index], pr.second.size());
+		d_objects_index += pr.second.size();
+	}
+	// Run the demo to make sure everything works.
 	runner<<<1,1>>>(d_objects, objs.size());
 }
 
